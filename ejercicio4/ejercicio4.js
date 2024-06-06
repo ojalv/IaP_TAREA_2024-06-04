515667631248f365a05703eb5f78abb6c30b7744
alvaro <oj.alv99@gmail.com>
2024-06-06 15:42:01 -0300

    ejercicio 4 beyond

--- ejercicio4/ejercicio4.js
+++ ejercicio4/ejercicio4.js
@@ -46,7 +46,7 @@ class CuentaBancaria {
         } else if (deposito === 0) {
             console.error("operacion invalida, no puede ingresar montos nulos")
         } else if (deposito > 0) {
-            this.saldo += deposito
+            this.saldo += parseFloat(deposito)
             this.historial.push(`+${deposito}`)
         } else {
             console.error("Error inesperado, operacion anulada");
@@ -59,7 +59,7 @@ class CuentaBancaria {
         } else if (retiro === 0) {
             console.error("operacion invalida, no puede ingresar montos nulos")
         } else if (retiro > 0 && this.saldo >= retiro) {
-            this.saldo -= retiro
+            this.saldo -= parseFloat(retiro)
             this.historial.push(`-${retiro}`)
         } else if (retiro > 0 && this.saldo < retiro) {
             console.error("Fondos insuficientes para realizar la operacion")
@@ -77,11 +77,136 @@ class CuentaBancaria {
 
 }
 
-const cuenta1 = new CuentaBancaria("pepe", 101)
+const c1 = new CuentaBancaria("alvaro", 100)
+const c2 = new CuentaBancaria("pedro", 5000)
+const c3 = new CuentaBancaria("pepe", 10000)
+const c4 = new CuentaBancaria("hector", 654)
+const c5 = new CuentaBancaria("jose", 90)
 
-cuenta1.consultarSaldo()
-cuenta1.depositar(200)
-cuenta1.consultarSaldo()
-cuenta1.retirar(300)
-cuenta1.consultarSaldo()
-cuenta1.mostrarHistorial()
+const cuentas = [c1, c2, c3, c4, c5]
+/*UI*/
+
+// crearCuenta variables
+const crearCuentaBtn = document.getElementById("crearCuentaBtn")
+const formularioNuevaCuenta = document.getElementById("formularioNuevaCuenta")
+const nombreTitular = document.getElementById("nombreTitularInp")
+const saldoInicial = document.getElementById("saldoInicialInp")
+const confirmarNuevaCuentaBtn = document.getElementById("confirmarNuevaCuentaBtn")
+const cancelarNuevaCuentaBtn = document.getElementById("cancelarNuevaCuentaBtn")
+const opacityForm = document.getElementById("opacityForm")
+
+// userPanel variables
+const userPanel = document.getElementById("userPanel")
+const nombrePanelUsuario = document.getElementById("nombrePanelUsuario")
+const saldoPanelUsuario = document.getElementById("saldoPanelUsuario")
+const historialPanelUsuario = document.getElementById("historialPanelUsuario")
+const opacityUserPanel = document.getElementById("opacityUserPanel")
+const volverBtn = document.getElementById("volverBtn")
+
+// renderizado de cuentas variables y funciones
+const cuentasRegistradas = document.getElementById("cuentasRegistradas")
+function renderizarCuentas(cuentas) {
+    let id = -1
+    cuentasRegistradas.innerHTML = ""
+    cuentas.forEach(c => {
+        id += 1
+        const { titular } = c
+        cuentasRegistradas.innerHTML += `
+        <div class="card" id="${id}">
+        <h3>${titular}</h3>
+        <span>cuenta Nro ${id}</span>
+        </div>
+        `
+    })
+    clickCardShowUserPanel()
+}
+
+// crear cuenta
+crearCuentaBtn.addEventListener("click", () => {
+    formularioNuevaCuenta.classList.remove("inactive")
+    opacityForm.classList.remove("inactive")
+})
+
+cancelarNuevaCuentaBtn.addEventListener("click", () => {
+    formularioNuevaCuenta.classList.add("inactive")
+    opacityForm.classList.add("inactive")
+})
+
+formularioNuevaCuenta.addEventListener("submit", (e) => {
+    e.preventDefault()
+})
+
+confirmarNuevaCuentaBtn.addEventListener("click", () => {
+    if (saldoInicial.value == "" || nombreTitular.value == "") {
+        alert("campos vacios")
+    } else if (saldoInicial.value < 0) {
+        alert("El saldo inicial de la cuenta no puede ser negativo")
+    } else if (saldoInicial.value >= 0) {
+        cuentas.push(new CuentaBancaria(nombreTitular.value, parseFloat(saldoInicial.value)))
+        nombreTitular.value = ""
+        saldoInicial.value = ""
+        formularioNuevaCuenta.classList.add("inactive")
+        opacityForm.classList.add("inactive")
+        renderizarCuentas(cuentas) //se renderizan las cuentas en #cuentasRegistradas
+    } else {
+        alert("Error inesperado, ingrese informacion valida");
+    }
+
+})
+
+// ver cuenta
+function clickCardShowUserPanel() {
+    cuentasRegistradas.querySelectorAll(".card").forEach(card => {
+        card.addEventListener("click", (e) => {
+            const id = e.target.closest(".card").id
+            userPanel.classList.remove("inactive")
+            opacityUserPanel.classList.remove("inactive")
+            renderizarPanelUsuario(id, cuentas)
+
+        })
+    })
+}
+
+function renderizarPanelUsuario(id, cuentas) {
+    historialPanelUsuario.innerHTML = ""
+    const { titular, saldo, historial } = cuentas[id]
+    nombrePanelUsuario.innerText = titular
+    saldoPanelUsuario.innerText = `$${saldo}`
+    document.getElementById("id-usuario").innerText = id
+    historial.forEach(o => {
+        historialPanelUsuario.innerHTML += `<div>${o}</div>`
+    })
+
+
+}
+
+
+volverBtn.addEventListener("click", () => {
+    opacityUserPanel.classList.add("inactive")
+    userPanel.classList.add("inactive")
+})
+
+
+// ingreso/retiro de dinero
+
+const extraccionBtn = document.getElementById("extraccionBtn")
+const depositoBtn = document.getElementById("depositoBtn")
+
+
+extraccionBtn.addEventListener("click", () => {
+    // id-usuario
+    let monto = prompt("ingrese monto")
+    let id = parseInt(document.getElementById("id-usuario").innerText)
+    cuentas[id].retirar(monto)
+    renderizarPanelUsuario(id, cuentas)
+})
+
+depositoBtn.addEventListener("click", () => {
+    // id-usuario
+    let monto = prompt("ingrese monto")
+    let id = parseInt(document.getElementById("id-usuario").innerText)
+    cuentas[id].depositar(monto)
+    renderizarPanelUsuario(id, cuentas)
+})
+
+renderizarCuentas(cuentas) 
